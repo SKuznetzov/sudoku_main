3bd8716a22dd1d1babb931c16a533e5635f3882a
SKuznetzov <s-kuznecov100@rambler.ru>
2022-04-13 20:22:22 +0300

    setAttribute max

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ for (let i = 0; i < squares; i++) {
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'number')
     inputElement.setAttribute('min','0')
+    inputElement.setAttribute('max','9')
 }
 
 
