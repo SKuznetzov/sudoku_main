3fa76b00af032fd1e1cbf6b1e137d4450462c7ca
SKuznetzov <s-kuznecov100@rambler.ru>
2022-04-13 20:20:23 +0300

    setAttribute

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const squares = 81
 for (let i = 0; i < squares; i++) {
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'number')
+    inputElement.setAttribute('min','0')
 }
 
 
